c85b9cfa286fbc889361227452a55dd817d26a7e
Waqee Khalid <waqee@live.com>
2015-03-09 15:35:42 -0300

    Added Multiword Team Name Input Support

--- src/Madness.cpp
+++ src/Madness.cpp
@@ -6,14 +6,22 @@ int main(int argc, char* argv[])
 {
 	SeasonManager madness;
 
+	string season_name = string(argv[2]);
+
+	string team_name = string(argv[3]);
+
+	//Handling multiple worded teams
+	for (int i = 4; i<argc; i++)
+		team_name += " " + string(argv[i]);
+
 	if (strcmp(argv[1], "rpiChampion") == 0)
-		madness.RPIChampion(string(argv[2]));
+		madness.RPIChampion(season_name);
 	else if (strcmp(argv[1], "list") == 0)
-		madness.ListGames(string(argv[2]), string(argv[3]));
+		madness.ListGames(season_name, team_name);
 	else if (strcmp(argv[1], "finalRecord") == 0)
-		madness.FinalRecord(string(argv[2]), string(argv[3]));
+		madness.FinalRecord(season_name, team_name);
 	else if (strcmp(argv[1], "bestWinPercentage") == 0)
-		madness.BestWinPercentage(string(argv[2]));
+		madness.BestWinPercentage(season_name);
 	else if (strcmp(argv[1], "largestMargin") == 0)
-		madness.LargestMargin(string(argv[2]));
+		madness.LargestMargin(season_name);
 }
